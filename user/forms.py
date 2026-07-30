b8d588b6b1ae41fb2d21ae8731d616a31ef793ed
Kauan <kauanmonteirosilva789@gmail.com>
2026-07-30 10:01:58 -0300

    adiciona templates e formulários para registro de usuário; implementa lógica de registro na view

---
+++ user/forms.py
@@ -0,0 +1,30 @@
+from django.forms import ModelForm
+from .models import User
+
+class UserRegisterForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if not username:
+            self.add_error('username', 'Username is required.')
+
+        if not password:
+            self.add_error('password', 'Password is required.')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data['username']
+        user.password = self.cleaned_data['password']
+
+        if commit:
+            user.save()
+
+        return user
+
+
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import UserRegisterForm
 
 def login_view(request):
     pass
@@ -7,7 +8,12 @@ def logout_view(request):
     pass
 
 def register_view(request):
-    pass    
+    form = UserRegisterForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('training:home') 
+    return render(request, 'pages/register_form.html', {'form': form})    
 
 def delete_user(request, user_id):
     pass
