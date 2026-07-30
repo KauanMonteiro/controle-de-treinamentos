000be47b6a6f5bc1a1658ecbd25afd249f9bbed0
Kauan <kauanmonteirosilva789@gmail.com>
2026-07-30 09:35:03 -0300

    adiciona modelos para Empresa, Funcionário, Departamento, Cargo e Tipo de Treinamento; implementa registros de treinamento.

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('user.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('user.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    company = models.ForeignKey('company.Company', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('user.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- training/models.py
+++ training/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class TrainingType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    revision = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class TrainingRegister(models.Model):
+    effectiveness_choices = [
+        ('E', 'Eficaz'),
+        ('I', 'Não eficaz'),
+    ]
+    training_type = models.ForeignKey(TrainingType, on_delete=models.CASCADE)
+    employee = models.ForeignKey('employee.Employee', on_delete=models.CASCADE)
+    docs = models.FileField(upload_to='training_docs/{employee_id.company.name}/{employee_id.name}/', null=True, blank=True)
+    effectiveness = models.CharField(max_length=1, choices=effectiveness_choices)
+    aplication_data_training = models.DateField()
+    instructor = models.CharField(max_length=100)
+    evaluator = models.CharField(max_length=100)
+    avaling_data_training = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('user.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.training_type.name} - {self.employee.name} - {self.employee.name}"
