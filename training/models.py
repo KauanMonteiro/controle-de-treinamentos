eec029a47683c3544127f59ee766800386582009
Kauan <kauanmonteirosilva789@gmail.com>
2026-08-01 15:46:18 -0300

    adiciona modelos de treinamento e suas migrações; atualiza URLs para visualização de treinamentos

--- training/models.py
+++ training/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class TrainingType(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=100, verbose_name='Nome do Treinamento')
+    codigo = models.CharField(max_length=20, unique=True, verbose_name='Código do Treinamento',null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey('user.User', on_delete=models.CASCADE)
     revision = models.IntegerField(default=1)
@@ -15,14 +15,14 @@ class TrainingRegister(models.Model):
         ('E', 'Eficaz'),
         ('I', 'Não eficaz'),
     ]
-    training_type = models.ForeignKey(TrainingType, on_delete=models.CASCADE)
-    employee = models.ForeignKey('employee.Employee', on_delete=models.CASCADE)
+    training_type = models.ForeignKey(TrainingType, on_delete=models.CASCADE, verbose_name='Tipo de Treinamento')
+    employee = models.ForeignKey('employee.Employee', on_delete=models.CASCADE, verbose_name='Funcionário')
     docs = models.FileField(upload_to='training_docs/{employee_id.company.name}/{employee_id.name}/', null=True, blank=True)
-    effectiveness = models.CharField(max_length=1, choices=effectiveness_choices)
-    aplication_data_training = models.DateField()
-    instructor = models.CharField(max_length=100)
-    evaluator = models.CharField(max_length=100)
-    avaling_data_training = models.DateField()
+    effectiveness = models.CharField(max_length=1, choices=effectiveness_choices, verbose_name='Eficácia')
+    aplication_data_training = models.DateField(verbose_name='Data de Aplicação do Treinamento')
+    instructor = models.CharField(max_length=100, verbose_name='Instrutor')
+    evaluator = models.CharField(max_length=100, verbose_name='Avaliador')
+    avaling_data_training = models.DateField(verbose_name='Data de Avaliação do Treinamento')
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey('user.User', on_delete=models.CASCADE)
 
--- training/urls.py
+++ training/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register_training/', views.register_training, name='register_training'),
+    path('trainigs_view/', views.trainings_view, name='trainings_view'),
 ]
